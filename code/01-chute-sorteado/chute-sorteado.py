a7d076010e13af34e672666138647fccc56741c1
Dario <dariogabriel113@gmail.com>
2019-08-01 16:17:26 -0300

    Cria 01-chute-sorteado

---
+++ code/01-chute-sorteado/chute-sorteado.py
@@ -0,0 +1,18 @@
+from random import randint
+
+print ('Bem vindo!')
+
+sorteado = randint(1, 100)
+
+chute = 0
+
+while chute != sorteado:
+    chute = int(input ('Chute: '))
+    if chute == sorteado:
+        print ('Você venceu!')
+    else:
+        if chute > sorteado:
+            print ('Alto')
+        else: 
+            print ('Baixo')
+print ('Fim do jogo!')
